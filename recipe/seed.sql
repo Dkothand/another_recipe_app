66cae2dbfff6dee97d8c70248b398b6f2e70fb15
Dkothand <dkothand@gmail.com>
2021-11-08 20:40:36 -0500

    Basic query to test seed-db command

--- recipe/seed.sql
+++ recipe/seed.sql
@@ -1,1 +1,20 @@
-SELECT "Testing seed file";
+/*
+Example recipe
+Recipe:
+name: Cheese Omelette
+description: A simple cheese omelette
+instructions: "Heat pan. Add butter, wait until butter foams. Beat eggs and add to pan"
+              "Push curds in gradually. Add cheese once it sets. Fold over and serve"
+
+ingredients:
+- 4 eggs
+- 1/4 cup cheese
+- butter
+
+*/
+
+INSERT INTO recipes (name, description, instructions, meal)
+    VALUES ("Cheese Omelette", "A simple cheese omelette",
+    "Heat pan. Add butter, wait until butter foams. Beat eggs and add to pan. \
+    Push curds in gradually. Add cheese once it sets. Fold over and serve",
+    "BREAKFAST")
