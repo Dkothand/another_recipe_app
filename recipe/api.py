295f984a4e2f1cf12c57839348de70d8809c1577
Dkothand <dkothand@gmail.com>
2021-11-09 20:30:43 -0500

    Starting work on POST request, placeholder code

--- recipe/api.py
+++ recipe/api.py
@@ -4,3 +4,8 @@ from recipe.db import query_db
 def get_recipes(id=None):
     res = query_db("SELECT * FROM recipes;")
     return res
+
+
+def create_recipe(request):
+    # TODO
+    return False, None
--- recipe/recipe.py
+++ recipe/recipe.py
@@ -1,10 +1,18 @@
-from flask import Blueprint
-from recipe.api import get_recipes
+from flask import Blueprint, jsonify, request
+from recipe.api import create_recipe, get_recipes
 
 
 bp = Blueprint('recipe', __name__, url_prefix='/recipe')
 
 
-@bp.route('/')
+@bp.route('/', methods=('GET', 'POST'))
 def recipe():
-    return get_recipes()
+    if request.method == 'GET':
+        return get_recipes()
+
+    if request.method == 'POST':
+        # TODO
+        msg, err = create_recipe(request)
+        if not err:
+            return jsonify({'code': 422, 'msg': 'Validation error'})
+        return jsonify({'code': 201, 'msg': 'Recipe created'})
